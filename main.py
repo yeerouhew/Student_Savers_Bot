bb06d606895499a08d15571fea3ba153a1ab9242
yeerouhw <yeerouhew@gmail.com>
2020-07-25 17:01:42 +0800

    add in new command

--- main.py
+++ main.py
@@ -956,12 +956,12 @@ def main():
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
-    # updater.start_webhook(listen="0.0.0.0",
-    #                       port=int(PORT),
-    #                       url_path=TOKEN)
+    # updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
     # updater.bot.setWebhook('https://student-saversbot.herokuapp.com/' + TOKEN)
-    # updater.bot.setWebhook('https://student-savers-testing24.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('https://student-savers-testing24.herokuapp.com/' + TOKEN)
 
 if __name__ == '__main__':
     main()
